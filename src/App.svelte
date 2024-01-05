06ee445d81f21a8ce141b3754b2790a41e05655b
Lyssieth <lys@lys.ee>
2024-01-05 15:45:07 +0200

    Move a big thing away

--- src/App.svelte
+++ src/App.svelte
@@ -4,52 +4,9 @@
   import ScrollButton from "./lib/ScrollButton.svelte";
   import { state } from "./lib/store";
   import { type Template } from "./lib/template";
+  import { exportQuestions } from "./lib/util";
 
   let exportBlock: HTMLTextAreaElement;
-  function exportQuestions() {
-    const sections = document.querySelectorAll(".section");
-    console.debug({ sections });
-    const exportText = Array.from(sections)
-      .map((section) => {
-        const sectionTitle = section.getAttribute("data-section");
-        const sectionDescription = section.querySelector(
-          ".section-description"
-        )?.textContent;
-        const questions = section.querySelectorAll(".question");
-        console.debug({ questions });
-
-        const questionTexts = Array.from(questions)
-          .map((question) => {
-            const questionText = question.getAttribute("data-question");
-            const answerEl: HTMLInputElement | HTMLTextAreaElement | null =
-              question.querySelector(".answer");
-            const answer = answerEl?.value;
-
-            console.debug({ questionText, answer });
-            if (answer) {
-              return `## ${questionText}\n${answer}`;
-            } else {
-              return null;
-            }
-          })
-          .join("\n");
-
-        console.debug({ questionTexts });
-
-        if (questionTexts.trim().length > 0) {
-          return `# ${sectionTitle}\n${sectionDescription}\n${questionTexts}`;
-        } else {
-          return null;
-        }
-      })
-      .join("\n\n");
-
-    if (exportText.trim().length > 0) {
-      exportBlock.value = exportText;
-    } else {
-      exportBlock.value = "";
-    }
-  }
 
   let changed: boolean = false;
   let questionnaire: Questionnaire;
@@ -149,7 +106,8 @@
     questions empty, and they won't show up. Same goes for sections.
   </p>
 
-  <button on:click={exportQuestions} disabled={state == null}>Refresh</button
+  <button on:click={() => exportQuestions(exportBlock)} disabled={state == null}
+    >Refresh</button
   ><br />
   <textarea
     bind:this={exportBlock}
---
+++ src/lib/util.ts
@@ -0,0 +1,44 @@
+export function exportQuestions(exportBlock: HTMLTextAreaElement) {
+  const sections = document.querySelectorAll(".section");
+  console.debug({ sections });
+  const exportText = Array.from(sections)
+    .map((section) => {
+      const sectionTitle = section.getAttribute("data-section");
+      const sectionDescription = section.querySelector(
+        ".section-description"
+      )?.textContent;
+      const questions = section.querySelectorAll(".question");
+      console.debug({ questions });
+
+      const questionTexts = Array.from(questions)
+        .map((question) => {
+          const questionText = question.getAttribute("data-question");
+          const answerEl: HTMLInputElement | HTMLTextAreaElement | null =
+            question.querySelector(".answer");
+          const answer = answerEl?.value;
+
+          console.debug({ questionText, answer });
+          if (answer) {
+            return `## ${questionText}\n${answer}`;
+          } else {
+            return null;
+          }
+        })
+        .join("\n");
+
+      console.debug({ questionTexts });
+
+      if (questionTexts.trim().length > 0) {
+        return `# ${sectionTitle}\n${sectionDescription}\n${questionTexts}`;
+      } else {
+        return null;
+      }
+    })
+    .join("\n\n");
+
+  if (exportText.trim().length > 0) {
+    exportBlock.value = exportText;
+  } else {
+    exportBlock.value = "";
+  }
+}
